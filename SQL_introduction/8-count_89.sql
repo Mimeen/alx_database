257aab3adce7efd9393018be2ef63224c91a9b3f
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-08-14 16:51:01 +0100

    cound id=89s

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,23 @@
 -- count the number of records
+/*
+-- Create database 
+DROP DATABASE IF EXISTS hbtn_test_db_8;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
+USE hbtn_test_db_8;
 
-SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
+-- Create records
+INSERT INTO first_table (id, name) VALUES (1, "A");
+INSERT INTO first_table (id, name) VALUES (89, "B");
+INSERT INTO first_table (id, name) VALUES (2, "C");
+INSERT INTO first_table (id, name) VALUES (89, "D");
+INSERT INTO first_table (id, name) VALUES (89, "E");
+INSERT INTO first_table (id, name) VALUES (3, "F");
+*/
+-- count first table columns with 89 ids
+SELECT COUNT(*) FROM first_table WHERE id = 89;

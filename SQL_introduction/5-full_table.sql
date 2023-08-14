e2693560fbcc0455b58c128a6b2c33b1b2fb7887
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-08-14 09:53:14 +0100

    show full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
--- lists all rows of the table
+-- show tables in the database
 
-SHOW CREATE TABLE hbtn_0c_0.first_table
+mysql -u your_username -p -e "SHOW CREATE TABLE hbtn_0c_0.first_table;" hbtn_0c_0 | tail -n 1

c09331cbee82e567062c26b9c4dc4d52ad67c880
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-08-14 07:26:39 +0100

    sql scripts

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,1 +1,3 @@
-SHOW DATABASES;
+-- this script lists all database in my workspace
+
+SHOW DATABASES
--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,3 @@
+-- This is to create a database that would not fail even if it exists already.
+
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

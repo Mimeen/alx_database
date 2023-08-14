bff58970b552d14c0416f8b0631db435ec22a392
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-08-14 12:54:40 +0100

    insert into table

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- inserts into the fisrt table
+
+-- INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (89, 'Holberton School')
+
+INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (89, 'Holberton School');

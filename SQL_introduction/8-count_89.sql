0f7f9e9cdc1546f4db447f699e5c71a57f821530
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-08-14 05:11:37 -0700

    count 89s

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,4 @@
--- count the number of records
+-- read new lines
 
-SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
+mysql -u your_username -p -e "SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;" hbtn_0c_0
+
